30d1419919b1cb466e713d7398e48cec0025c122
wsli <99927800@qq.com>
2016-11-02 16:06:29 +0800

    loading scene test

--- Classes/Scenes/StartingScene.cpp
+++ Classes/Scenes/StartingScene.cpp
@@ -1,95 +1,69 @@
 #include "StartingScene.h"
 #include "SimpleAudioEngine.h"
+#include "AppMacros.h"
 
 USING_NS_CC;
 
+#pragma makr - init scene
 Scene* Starting::createScene()
 {
-    // 'scene' is an autorelease object
-    auto scene = Scene::create();
-    
-    // 'layer' is an autorelease object
-    auto layer = Starting::create();
-
-    // add layer as a child to scene
-    scene->addChild(layer);
-
-    // return the scene
-    return scene;
+        auto scene = Scene::create();
+        auto layer = Starting::create();
+        scene->addChild(layer);
+        
+        return scene;
 }
 
-// on "init" you need to initialize your instance
 bool Starting::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Layer::init() )
-    {
-        return false;
-    }
+        if (!Layer::init()){
+                return false;
+        }
     
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(Starting::menuCloseCallback, this));
-    
-    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
+        auto visibleSize = Director::getInstance()->getVisibleSize();
+        Vec2 origin = Director::getInstance()->getVisibleOrigin();
+        
+        auto sprite = Sprite::create("StartingLogo.png");
+        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+        this->addChild(sprite, 0);
+        
+        
+        _loadingBar = LoadingBar::create("sliderProgress.png");
+        _loadingBar->setDirection(LoadingBar::Direction::LEFT);
+        _loadingBar->setPercent(0);
+        this->addChild(_loadingBar, 1);
+        
+        auto label = Label::createWithTTF("Loading", "fonts/Marker Felt.ttf", 24);
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height - label->getContentSize().height));
+        this->addChild(label, 2);
     
     return true;
 }
 
 
-void Starting::menuCloseCallback(Ref* pSender)
-{
-    //Close the cocos2d-x game scene and quit the application
-    Director::getInstance()->end();
+#pragma makr - refresh loading bars
+void Starting::onEnter(){
+        _count = 0;
+        
+        scheduleUpdate();
+        
+        loadingResource();
+}
 
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
-    
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
-    
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
-    
-    
+void Starting::update(float delta){
+        
+        
+        _loadingBar->setPercent(_count);
+}
+
+void Starting::onExit(){
+        unscheduleUpdate();
+}
+
+#pragma makr - loading resources
+void Starting::loadingResource(){
+        
+        CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect(EFFECT_FILE);
+        _count = 50;
 }

c39a8325d9fa96368236de096e81808dff5171bc
songs <songsofgrazier@hotmail.com>
2016-11-06 12:48:50 +0800

    integrate face book

--- proj.ios_mac/ios/AppController.mm
+++ proj.ios_mac/ios/AppController.mm
@@ -23,9 +23,12 @@
  ****************************************************************************/
 
 #import "AppController.h"
+#import "platform/ios/CCEAGLView-ios.h"
 #import "cocos2d.h"
 #import "AppDelegate.hpp"
 #import "RootViewController.h"
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 @implementation AppController
 
@@ -38,34 +41,58 @@
 static AppDelegate s_sharedApplication;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
-    // Override point for customization after application launch.
-
-    // Add the view controller's view to the window and display.
-    window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
-
-    // Use RootViewController to manage CCEAGLView
-    _viewController = [[RootViewController alloc]init];
-    _viewController.wantsFullScreenLayout = YES;
-    
-
-    // Set RootViewController to window
-    if ( [[UIDevice currentDevice].systemVersion floatValue] < 6.0)
-    {
-        // warning: addSubView doesn't work on iOS6
-        [window addSubview: _viewController.view];
-    }
-    else
-    {
-        // use this method on ios6
-        [window setRootViewController:_viewController];
-    }
-
-    [window makeKeyAndVisible];
-
-    [[UIApplication sharedApplication] setStatusBarHidden:true];
-
-
-    return YES;
+        cocos2d::Application *app = cocos2d::Application::getInstance();
+        app->initGLContextAttrs();
+        cocos2d::GLViewImpl::convertAttrs();
+        
+        // Override point for customization after application launch.
+        
+        // Add the view controller's view to the window and display.
+        window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
+        
+        // Init the CCEAGLView
+        CCEAGLView *eaglView = [CCEAGLView viewWithFrame: [window bounds]
+                                             pixelFormat: (NSString*)cocos2d::GLViewImpl::_pixelFormat
+                                             depthFormat: cocos2d::GLViewImpl::_depthFormat
+                                      preserveBackbuffer: NO
+                                              sharegroup: nil
+                                           multiSampling: NO
+                                         numberOfSamples: 0 ];
+        
+        // Enable or disable multiple touches
+        [eaglView setMultipleTouchEnabled:NO];
+        
+        // Use RootViewController manage CCEAGLView
+        _viewController = [[RootViewController alloc] initWithNibName:nil bundle:nil];
+        
+        _viewController.view = eaglView;
+        
+        // Set RootViewController to window
+        if ( [[UIDevice currentDevice].systemVersion floatValue] < 6.0)
+        {
+                // warning: addSubView doesn't work on iOS6
+                [window addSubview: _viewController.view];
+        }
+        else
+        {
+                // use this method on ios6
+                [window setRootViewController:_viewController];
+        }
+        
+        [window makeKeyAndVisible];
+        
+        [[UIApplication sharedApplication] setStatusBarHidden:true];
+        
+        // IMPORTANT: Setting the GLView should be done after creating the RootViewController
+        cocos2d::GLView *glview = cocos2d::GLViewImpl::createWithEAGLView(eaglView);
+        cocos2d::Director::getInstance()->setOpenGLView(glview);
+        
+        bool ret= [[FBSDKApplicationDelegate sharedInstance] application:application
+                                           didFinishLaunchingWithOptions:launchOptions];
+        
+        app->run();
+        
+        return ret;
 }
 
 
@@ -78,12 +105,24 @@ static AppDelegate s_sharedApplication;
     /* cocos2d::Director::getInstance()->pause(); */
 }
 
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+        return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                              openURL:url
+                                                    sourceApplication:sourceApplication
+                                                           annotation:annotation];
+}
+
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
      //We don't need to call this method any more. It will interupt user defined game pause&resume logic
     /* cocos2d::Director::getInstance()->resume(); */
+        
+        [FBSDKAppEvents activateApp];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {

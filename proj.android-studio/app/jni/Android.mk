3e3f97c0dbebab88211b7bdc7ca29c2f71170024
wsli <99927800@qq.com>
2016-11-10 12:52:37 +0800

    android version

--- proj.android-studio/app/jni/Android.mk
+++ proj.android-studio/app/jni/Android.mk
@@ -6,16 +6,45 @@ $(call import-add-path,$(LOCAL_PATH)/../../../cocos2d)
 $(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/external)
 $(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/cocos)
 $(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/cocos/audio/include)
+$(call import-add-path,$(LOCAL_PATH))
 
 LOCAL_MODULE := MyGame_shared
 
 LOCAL_MODULE_FILENAME := libMyGame
 
+LOCAL_WHOLE_STATIC_LIBRARIES += PluginIAP
+LOCAL_WHOLE_STATIC_LIBRARIES += PluginFacebook
+LOCAL_WHOLE_STATIC_LIBRARIES += sdkbox
+
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../../Classes/AppDelegate.cpp \
-                   ../../../Classes/HelloWorldScene.cpp
+                   ../../../Classes/Scenes/SplashScene.cpp \
+                   ../../../Classes/Scenes/StartingScene.cpp \
+                   ../../../Classes/Scenes/LevelSelectScene.cpp \
+                   ../../../Classes/Scenes/GameScene.cpp \
+                   ../../../Classes/RandomMap/RandomMap.cpp \
+                   ../../../Classes/RandomMap/ScreenCoordinate.cpp \
+                   ../../../Classes/RandomMap/MapCreator.cpp \
+                   ../../../Classes/RandomMap/MapBasicInfo.cpp \
+                   ../../../Classes/GameLogic/AreaData.cpp \
+                   ../../../Classes/GameLogic/DiceGame.cpp \
+                   ../../../Classes/GameLogic/GameAI.cpp \
+                   ../../../Classes/GameLogic/GameData.cpp \
+                   ../../../Classes/GameLogic/GamePlayer.cpp \
+                   ../../../Classes/GameLogic/JoinData.cpp \
+                   ../../../Classes/Common/GameHelpScene.cpp \
+                   ../../../Classes/Common/GamePayUtil.cpp \
+                   ../../../Classes/Common/GameSettingsScene.cpp \
+                   ../../../Classes/Common/PopUpBaseDialog.cpp \
+                   ../../../Classes/Common/PopUpOkCancelDialog.cpp \
+                   ../../../Classes/Common/PopUpOkDialog.cpp \
+                   ../../../Classes/Common/WebSocktUtil.cpp 
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes \
+					$(LOCAL_PATH)/../../../Classes/Common \
+					$(LOCAL_PATH)/../../../Classes/GameLogic \
+					$(LOCAL_PATH)/../../../Classes/RandomMap \
+					$(LOCAL_PATH)/../../../Classes/Scenes
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
@@ -29,6 +58,9 @@ LOCAL_STATIC_LIBRARIES := cocos2dx_static
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,.)
+$(call import-module, ./sdkbox)
+$(call import-module, ./pluginiap)
+$(call import-module, ./pluginfacebook)
 
 # _COCOS_LIB_IMPORT_ANDROID_BEGIN
 # _COCOS_LIB_IMPORT_ANDROID_END

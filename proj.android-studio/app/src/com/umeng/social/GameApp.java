b28db8afa831059a279b7eab87373888409d3e71
wsli <99927800@qq.com>
2017-02-15 15:47:07 +0800

    s

--- proj.android-studio/app/src/com/umeng/social/GameApp.java
+++ proj.android-studio/app/src/com/umeng/social/GameApp.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.umeng.socialize.Config;
 import com.umeng.socialize.PlatformConfig;
-import com.umeng.socialize.UMShareAPI;
 
 /**
  * Created by wsli on 17/2/15.
@@ -19,6 +18,5 @@ public class GameApp extends Application {
         super.onCreate();
         //开启debug模式，方便定位错误，具体错误检查方式可以查看http://dev.umeng.com/social/android/quick-integration的报错必看，正式发布，请关闭该模式
         Config.DEBUG = true;
-        UMShareAPI.get(this);
     }
 }

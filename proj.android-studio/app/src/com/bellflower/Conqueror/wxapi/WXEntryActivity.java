ab1c1dc2fa5f8c0bf89a6df514b5297aba4bb530
wsli <99927800@qq.com>
2017-02-15 15:47:51 +0800

    s

---
+++ proj.android-studio/app/src/com/bellflower/Conqueror/wxapi/WXEntryActivity.java
@@ -0,0 +1,12 @@
+package com.bellflower.Conqueror.wxapi;
+
+
+import com.umeng.weixin.callback.WXCallbackActivity;
+
+//import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity {
+
+
+
+}
